a41011f2bd5581a69fb3de56ab17118cca847b2a
chaoyee <chaoyee22@gmail.com>
2014-08-12 01:20:42 +0800

    Add translations

--- app/overrides/add_link_to_users_favorite_products.rb
+++ app/overrides/add_link_to_users_favorite_products.rb
@@ -2,7 +2,7 @@ Deface::Override.new(
   :virtual_path => "spree/shared/_nav_bar",
   :name => "add_link_to_users_favorite_products",
   :insert_before => "li#search-bar",
-  :text => "<% if spree_current_user %><li><%= link_to 'Favorite Products', favorite_products_path %></li><% end %>",
+  :text => "<% if spree_current_user %><li><%= link_to Spree.t(:favorite_products), favorite_products_path %></li><% end %>",
   :sequence => { :before => "auth_shared_login_bar"}
 )
 
